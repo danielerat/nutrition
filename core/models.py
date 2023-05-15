fd293318234a114bbcdc6af950ec04da088d64c6
danielerat <davidodo2015@gmail.com>
2023-05-15 23:15:44 +0200

    Adding the appointment model

---
+++ core/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.conf import settings
+
+MEAL_TYPE_CHOICES = (
+    ('breakfast', 'Breakfast'),
+    ('lunch', 'Lunch'),
+    ('dinner', 'Dinner'),
+    ('snack', 'Snack'),
+)
+BLOOD_TYPE_CHOICES = (
+    ('A+', 'A+'),
+    ('A-', 'A-'),
+    ('B+', 'B+'),
+    ('B-', 'B-'),
+    ('AB+', 'AB+'),
+    ('AB-', 'AB-'),
+    ('O+', 'O+'),
+    ('O-', 'O-'),
+)
+STATUS_CHOICES = (
+    ('pending', 'Pending'),
+    ('confirmed', 'Confirmed'),
+    ('completed', 'Completed'),
+    ('cancelled', 'Cancelled'),
+)
+REVIEW_PROCESS_STATUS = (
+    ('pending', 'Pending'),
+    ('reviewed', 'Reviewed'),
+    ('confirmed', 'Confirmed'),
+    ('cancelled', 'Cancelled'),
+)
+
+
+class Appointment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(
+        max_length=15, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
