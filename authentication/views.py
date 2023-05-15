fb030b57a59c1c4871a470ae577d165f060df253
danielerat <davidodo2015@gmail.com>
2023-05-15 23:01:58 +0200

    Adding The Login End point with token generation

--- authentication/views.py
+++ authentication/views.py
@@ -22,3 +22,38 @@ class RegisterAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    authentication_classes = []
+
+    def post(self, request):
+        phone_number = request.data['phone_number']
+        password = request.data['password']
+
+        user = User.objects.filter(phone_number=phone_number).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed("Invalid Credentials")
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed("Invalid Credentials")
+
+        # reate an access Token
+        access_token = create_access_token(user.id)
+        refresh_token = create_refresh_token(user.id)
+
+        # Store in the user token db
+        UserToken.objects.create(
+            user_id=user.id,
+            token=refresh_token,
+            expired_at=datetime.datetime.utcnow() + datetime.timedelta(days=7)
+        )
+
+        response = Response()
+        response.set_cookie(key='refresh_token',
+                            value=refresh_token, httponly=True)
+        response.data = {
+            'token': access_token
+        }
+
+        return response
