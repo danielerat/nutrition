6b6f488dc7a727d5d9d5661d1d89b37ba0980d79
danielerat <davidodo2015@gmail.com>
2023-05-17 10:06:50 +0200

    Rgistering Pending Appoitment to the admin Section

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from appointment.models import AppointmentApplication
 
-# Register your models here.
+
+@admin.register(AppointmentApplication)
+class AppointmentApplicationAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "title",
+        "date",
+        "status",
+        "created_at"
+    ]
+    list_per_page = 20
+    list_select_related = ["user"]
+    ordering = ["date", "created_at"]
+    search_fields = [
+        "user__first_name", "user__last_name", "user__phone_number"
+    ]
+    list_filter = ['status', 'date', 'created_at']
