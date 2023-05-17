2abe52ccc3468239a6b512f4f65cc08e0ec2c81c
danielerat <davidodo2015@gmail.com>
2023-05-17 15:08:37 +0200

    Adding The Signal To create the appointment logic.

--- appointment/signals/handlers.py
+++ appointment/signals/handlers.py
@@ -1,8 +1,10 @@
 from django.db.models.signals import post_save
 from appointment.models import AppointmentApplication
-from django.dispatch import receiver
+from rest_framework.exceptions import ValidationError
 
-from nutrition.utils.send_email_templates import send_accepted_appointment_email
+from django.dispatch import receiver
+from rest_framework.views import exception_handler
+from nutrition.utils.send_email_templates import send_accepted_appointment_email, send_cancelled_appointment_email, send_declined_appointment_email
 # from nutrition.utils.send_text_message import send_text_message_welcome
 
 from django.db import transaction
@@ -14,15 +16,24 @@ from core.models import Appointment
 def update(sender, instance, created, **kwargs):
     if not created:
         if instance.status == "accepted":
-
-            with transaction.atomic():
-                send_accepted_appointment_email(
-                    instance.user.email, instance.user.get_full_name())
-                Appointment.objects.create(
-                    user=instance.user,
-                    title=instance.title,
-                    description=instance.description,
-                    start_time=instance.date,
-                )
-
-                instance.delete()
+            try:
+                with transaction.atomic():
+                    Appointment.objects.create(
+                        user=instance.user,
+                        title=instance.title,
+                        description=instance.description,
+                        start_time=instance.date,
+                    )
+                    instance.delete()
+                    send_accepted_appointment_email(
+                        instance.user.email, instance.user.get_full_name())
+            except:
+                raise ValidationError("Something went Wrong Try again later.")
+        elif instance.status == "declined":
+            send_declined_appointment_email(
+                instance.user.email, instance.user.get_full_name())
+            instance.delete()
+        elif instance.status == "cancelled":
+            send_cancelled_appointment_email(
+                instance.user.email, instance.user.get_full_name())
+            instance.delete()
