36dd79ba100c030b2e53c03b7f03c92d23f54907
danielerat <davidodo2015@gmail.com>
2023-05-17 10:08:56 +0200

    Registering the application appoitment endpoint

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
 from django.shortcuts import render
+from appointment.models import AppointmentApplication
+from appointment.serializers import AppointmentSerializer
 
-# Create your views here.
+
+class AppointmentViewset(ModelViewSet):
+    permission_classes = []
+    serializer_class = AppointmentSerializer
+    queryset = AppointmentApplication.objects.all()
