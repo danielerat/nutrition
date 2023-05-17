dcda12d0ce22a9e388b23cdccc9fc25bc175a8ee
danielerat <davidodo2015@gmail.com>
2023-05-17 15:52:47 +0200

    Adding the Mealplan to the admin section

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Appointment
+from .models import Appointment, MealPlan, Meal
+from django.db import models
 
 
 @admin.register(Appointment)
@@ -19,3 +20,29 @@ class AppointmentAdmin(admin.ModelAdmin):
         "user__first_name", "user__last_name", "user__phone_number"
     ]
     list_filter = ['status',  'created_at']
+
+
+class MealInline(admin.TabularInline):
+    model = Meal
+    min_num = 0
+    max_num = 10
+    extra = 2
+
+
+@admin.register(MealPlan)
+class MealPlanAdmin(admin.ModelAdmin):
+    list_display = [
+        "chef",
+        "patient",
+        "title",
+        "status",
+        "created_at",
+    ]
+    list_per_page = 20
+    list_prefe = ["patient", "chef"]
+    ordering = ["created_at"]
+    search_fields = [
+        "user__first_name", "user__last_name", "user__phone_number"
+    ]
+    list_filter = ['chef', 'status',  'created_at']
+    inlines = [MealInline]
