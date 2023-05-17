a71b3014c71edb1e36cf0d99b3cb54f25f553cb4
danielerat <davidodo2015@gmail.com>
2023-05-17 10:09:43 +0200

    Adding the Appointment model to the admin section

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "title",
+        "start_time",
+        "end_time",
+        "status",
+        "created_at"
+    ]
+    list_per_page = 20
+    list_select_related = ["user"]
+    ordering = ["created_at"]
+    search_fields = [
+        "user__first_name", "user__last_name", "user__phone_number"
+    ]
+    list_filter = ['status',  'created_at']
