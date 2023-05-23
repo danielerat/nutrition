92f05919524e221a9afd3cc0938b9d21bca2d283
danielerat <davidodo2015@gmail.com>
2023-05-23 16:45:50 +0200

    Creating the mealplan Viewset and overide the create method of the viewset

--- core/views.py
+++ core/views.py
@@ -1,14 +1,23 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Appointment,MealPlan
-from .serializers import AppointmentSerializer,MealPlanSerializer
-
+from .models import Appointment, Health, Meal,MealPlan
+from authentication.models import User
+from .serializers import AppointmentSerializer, CreateMealPlanSerializer, CreateMealSerializer, HealthSerialzier,MealPlanSerializer, MealSerializer,PatientSerializer
+from rest_framework import  status
+from rest_framework.response import Response
 
 class AppointmentViewset(ModelViewSet):
     permission_classes = []
     serializer_class = AppointmentSerializer
     queryset = Appointment.objects.all()
 
-class MealplanViewset(ModelViewSet):
-    permission_classes = []
+
+class MealPlanViewset(ModelViewSet):
     serializer_class = MealPlanSerializer
-    queryset = MealPlan.objects.all()
+    def get_queryset(self):
+        return MealPlan.objects.filter()
+    def create(self, request, *args, **kwargs):
+        serializer = CreateMealPlanSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
