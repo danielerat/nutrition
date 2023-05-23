ed3a70da90c470b821a384738865397a29ac84d8
danielerat <davidodo2015@gmail.com>
2023-05-23 16:48:00 +0200

    Registering our sub userls for mealplans and sub_sub Url for meal

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import AppointmentViewset, MealplanViewset
+from .views import AppointmentViewset, HealthViewset, MealPlanViewset, MealViewset, MealplanViewset, PatientViewset
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
@@ -7,6 +7,20 @@ router.register('appointment', AppointmentViewset,
                 basename='appointments')
 router.register('mealplans', MealplanViewset,
                 basename='mealplans')
+router.register('me', PatientViewset,
+                basename='patients')
+
+me_router = routers.NestedSimpleRouter(
+    router, 'me', lookup='patient')
+me_router.register('health', HealthViewset, basename='patient-health')
+me_router.register('mealplan', MealPlanViewset, basename='patient-mealplan',)
+
+meal_router = routers.NestedSimpleRouter(me_router, 'mealplan', lookup='mealplan')
+meal_router.register('meal', MealViewset, basename='meal')
+
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(me_router.urls)),
+    path('', include(meal_router.urls)),
 ]
