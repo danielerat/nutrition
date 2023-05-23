73e5ffd82bbcc5ee836f3f2e0ba4f5fe3666e9f5
danielerat <davidodo2015@gmail.com>
2023-05-23 10:33:26 +0200

    Creating the mealplanViewset to get all meals and stuff

--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
-from django.shortcuts import render
-from .models import Appointment
-from .serializers import AppointmentSerializer
+from .models import Appointment,MealPlan
+from .serializers import AppointmentSerializer,MealPlanSerializer
 
 
 class AppointmentViewset(ModelViewSet):
     permission_classes = []
     serializer_class = AppointmentSerializer
     queryset = Appointment.objects.all()
+
+class MealplanViewset(ModelViewSet):
+    permission_classes = []
+    serializer_class = MealPlanSerializer
+    queryset = MealPlan.objects.all()
