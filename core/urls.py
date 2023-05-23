935151ed39f27c496b42cc5a9a332002803c6599
danielerat <davidodo2015@gmail.com>
2023-05-23 10:33:54 +0200

    Registering the ednpoint to the admin section

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from .views import AppointmentViewset
+from .views import AppointmentViewset, MealplanViewset
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register('appointment', AppointmentViewset,
                 basename='appointments')
+router.register('mealplans', MealplanViewset,
+                basename='mealplans')
 urlpatterns = [
     path('', include(router.urls)),
 ]
