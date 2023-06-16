369ca14c119d555c09a773952b02572cc8cbf31d
danielerat <davidodo2015@gmail.com>
2023-06-16 10:31:29 +0200

    Adding Our models for the chat app and the related names

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Conversation(models.Model):
+    doctor = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='doctor_conversations'
+    )
+    patient = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='patient_conversations'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name='messages'
+    )
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
